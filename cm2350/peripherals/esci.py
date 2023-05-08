dcb2ea1cc6940828e0a2786e9a080257fc62be39
thomas.somerville <thomas.somerville@grimm-co.com>
2023-05-08 18:27:50 -0400

    Finished commenting all bit fields if emulation needed or not

--- cm2350/peripherals/esci.py
+++ cm2350/peripherals/esci.py
@@ -34,59 +34,93 @@ ESCI_LRR_OFFSET    = 0x0014
 ESCI_LPR_OFFSET    = 0x0018
 
 
-#Happy with how this one looks
+#No Fancy Dancy Needed
 class ESCI_x_BRR(PeriphRegister):
     def __init__(self):
         super().__init__()
         self._pad1 = v_const(3, 0b000)
+        #Needs special handling for the isntance of lower byte written two without preceding byte, if upper byte has a non zero value. 
         self.sbr = v_bits(13, 0b0000000000100)
 
-#This register has horizontal split for the RWU bit space also shares with rwm.
+
 class ESCI_x_CR1(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #This bit along with RSCR need special handling.
         self.loops = v_bits(1)
         self._pad1 = v_const(1)
+        #This bit along with loops need special handling.
         self.rsrc = v_bits(1)
+        #This sets start and stop bits for frames. I dont believe this is needed since we wont
         self.m = v_bits(1)
+        #I believe ther will be no sleep, so no wake needeD?
         self.wake = v_bits(1)
+        #Needs special handling - only to throw exception that sleep / wake is not a supported feature.
         self.ilt = v_bits(1)
+        #no frames so no parity biuts needed?
         self.pe = v_bits(1)
+        #again, no frames no parity?
         self.pt = v_bits(1)
+        #Interrupt not needed per erin
         self.tie = v_bits(1)
+        #Interrupt not needed per erin
         self.tcie = v_bits(1)
+        #Interrup not needed per erin
         self.rie = v_bits(1)
+        #Interrupt not needed per erin
         self.ilie = v_bits(1)
+        #Interrupt not needed per erin
         self.te = v_bits(1)
+        #Special handling Needed to determine  if enabled then process message, if disabled, ignore it.
         self.re = v_bits(1)
+        #no wakeup mode will be in place, not needed?
         self.rwu = v_bits(1)
+        #This sends determines if a break character is set. I dont believe we will need to have a break charcater ever to denote end of stream and return to ready state mode?
         self.sbk = v_bits(1)
 
 #Haoppy with how this one looks.
 class ESCI_x_CR2(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #Needs special handling, if enabled, process message, if disabled ignore.
         self.mdis = v_bits(1)
+        #This is transmission error detection. This looks like layer 2 err checking and thus not needeD?
         self.fbr = v_bits(1)
+        #Review with Erin, I didnt get DMA
         self.bstp = v_bits(1, 0b1)
+        #This bit controls the BERR interrupt request generation, but is not hte interrupt itself. Review with Erin, how to handle this?
         self.berrie = v_bits(1)
+        #Probably needs special handling
         self.rxdma = v_bits(1)
+        #Probvably needs special handling
         self.txdma = v_bits(1)
+        #Break Character length, I dont believe we need break characters?
         self.brcl = v_bits(1)
+        #This enabled / disabled the TXD pin. layer 1 stuff not needed?
         self.txdir = v_bits(1)
+        #This is fast bit err detection. Sounds like layer 2 stuff not needed?
         self.besm = v_bits(1)
+        #Interrupt not needed per erin
         self.bestp = v_bits(1)
+        #Pin Polarity?! have Erinr eview with me, but I dont believe it is needed
         self.rxpol = v_bits(1)
+        #Parity bit masking. parity bits not used/ therefore this not needed
         self.pmsk = v_bits(1)
+        #Interrupt not needed per erin
         self.orie = v_bits(1)
+        #Interrupt not needed per erin
         self.nfie = v_bits(1)
+        #Interrupt not needed per erin
         self.feie = v_bits(1)
+        #Interrupt not needed per erin
         self.pfie = v_bits(1)
+        #Interrupt not needed per erin
 
 #This register has the horizontal split fgor RD/TD. needs more work
 class ESCI_x_DR(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #This entire register will need special handling
         self.rn  = v_const(1)
         self.tn  = v_bits(1)
         self.err = v_const(1)
@@ -115,15 +149,15 @@ class ESCI_x_IFSR1(PeriphRegister):
         self.feie  = v_w1c(1)
         #renamed from pf to pfie for interrupt processing
         self.pf  = v_w1c(1)
-        
+        #pad remains pad
         self._pad1  = v_const(3)
         #renamed from berr to bestp for interrupt processing
         self.bestp  = v_w1c(1)
-        
+        #pad remains pad
         self._pad2  = v_const(2)
-        
+        #Transmitter active, status bit is set as long as transmmittion of frame is active. Likely dont need special handling.
         self.tact  = v_const(1)
-        
+        #Receiver active, bit set during reception of data. likely wont need special handling?
         self.ract  = v_const(1)
 
 class ESCI_x_IFSR2(PeriphRegister):
@@ -145,7 +179,7 @@ class ESCI_x_IFSR2(PeriphRegister):
         self.ckie  = v_w1c(1)
         #renamed from frc to fcie for interrupt processing
         self.fcie  = v_w1c(1)
-        
+        #pad remains pad
         self._pad1  = v_const(6)
         #renamed from ureq to uqie for interrupt processing
         self.uqie  = v_w1c(1)
@@ -155,19 +189,33 @@ class ESCI_x_IFSR2(PeriphRegister):
 class ESCI_x_LCR1(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #Will require special handling. Lin enginer in operation / idle
         self.lres  = v_bits(1)
+        #LIN wake up trigger, not needed as no wake up
         self.wu  = v_bits(1)
+        #LIN wake up delimiter time, not needeed no wake up
         self.wud  = v_bits(2)
+        #pad remains pad
         self._pad1  = v_const(2)
+        #controls 2x parity bits in LIN header, not neede since no headers
         self.prty  = v_bits(1)
+        #needs special handling. 0 = sci mode, 1 = LIN mode
         self.lin  = v_bits(1)
+        #this looks a LOT like a normal interrupt. Run apst erin why it is not in the interrupt table
         self.rxie  = v_bits(1)
+        #This looks a lot lieka normal interrupt, ask erin why not in interrupt table
         self.txie  = v_bits(1)
+        #Wakeup interrupt bit, no wake up, not needed
         self.wuie  = v_bits(1)
+        #ask erin why this is not in interrupt table
         self.stie  = v_bits(1)
+        #ask erin why this is not in interrupt table
         self.pbie  = v_bits(1)
+        ##ask erin why this is not in interrupt table
         self.cie  = v_bits(1)
+        ##ask erin why this is not in interrupt table
         self.ckie  = v_bits(1)
+        #ask erin why this is not in interrupt table
         self.fcie  = v_bits(1)
 
 #Happy with how this one looks
@@ -175,45 +223,55 @@ class ESCI_x_LCR2(PeriphRegister):
     def __init__(self):
         super().__init__()
         self._pad1 = v_const(6)
+        #ask erin why this is not in interrupt table
         self.uqie = v_bits(1)
+        #ask erin why this is not in interrupt table
         self.ofie  = v_bits(1)
+        #pad remains pad
         self._pad2  = v_const(8)
 
 #Ummm this one is confusing. Wait for Erin
 class ESCI_x_LTR(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #whole register will require special handling
         self.filler = v_bits(8)
 
 #Happy with hose this looks?
 class ESCI_x_LRR(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #whole register will require special handling
         self.d   = v_const(8)
 
 #happy with how this looks.
 class ESCI_x_LPR(PeriphRegister):
     def __init__(self):
         super().__init__()
+        #no descrion or details on this?
         self.p  = v_bits(16, 0b1100010110011001)
 
 class ESCI_x_CR3(PeriphRegister):
     def __init__(self):
         super().__init__()
         self._pad1 = v_const(3)
+        #Syncronization sequence not needed?
         self.synm = v_bits(1)
+        #noty sure about overruns
         self.eroe = v_bits(1)
+        #not sure
         self.erfe = v_bits(1)
+        #Probably not needed, double check
         self.erpe = v_bits(1)
+        #will require special handling
         self.m2 = v_bits(1)
+        #pad remains pad
         self._pad2 = v_const(8)
 
 
 class ESCI_REGISTERS(PeripheralRegisterSet):
     def __init__(self):
         super().__init__()
-
-
         self.brr = (ESCI_BRR_OFFSET, ESCI_x_BRR())
         self.cr1 = (ESCI_CR1_OFFSET, ESCI_x_CR1())
         self.cr2 = (ESCI_CR2_OFFSET, ESCI_x_CR2()) 
